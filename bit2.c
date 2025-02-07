d804ac9b8a156b5ef3d428d0169ffbba0ea30472
Euluer01 <144732266+Euluer01@users.noreply.github.com>
2025-02-07 11:37:19 -0500

    Add files via upload

--- bit2.c
+++ bit2.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include "assert.h"
 #include "bit2.h"
+#include "bit.h"
 
-
-
+struct Bit2_T {
+    int rows;
+    int cols;
+    Bit_T bit_arr;
+};
 
 Bit2_T Bit2_new(int rows, int cols) 
 {
-    Bit2_T bit2;
-    bit2.bit_arr = Bit_new(rows * cols);
-    assert( bit2.bit_arr != NULL);
-    bit2.rows = rows;
-    bit2.cols = cols;
+    Bit2_T bit2 = malloc(sizeof(*bit2));
+    assert(bit2 != NULL);
+    bit2->bit_arr = Bit_new(rows * cols);
+    assert( bit2->bit_arr != NULL);
+    bit2->rows = rows;
+    bit2->cols = cols;
 
     return bit2;
 }
 
-int Bit2_put(Bit2_T *bit2, int row, int col, int bit)
+int Bit2_put(Bit2_T bit2, int row, int col, int bit)
 {
+    assert(bit2 != NULL);
     assert(row >= 0 && row < bit2->rows);
     assert(col >= 0 && col < bit2->cols);
     int index = row * bit2->cols + col;
@@ -29,40 +35,44 @@ int Bit2_put(Bit2_T *bit2, int row, int col, int bit)
 
 int Bit2_get(Bit2_T bit2, int row, int col)
 {
-    assert(row >= 0 && row < bit2.rows);
-    assert(col >= 0 && col < bit2.cols);
-    int index = row * bit2.cols + col;
-    int curr_bit = Bit_get(bit2.bit_arr, index);
+    assert(bit2 != NULL);
+    assert(row >= 0 && row < bit2->rows);
+    assert(col >= 0 && col < bit2->cols);
+    int index = row * bit2->cols + col;
+    int curr_bit = Bit_get(bit2->bit_arr, index);
     
     return curr_bit;
 }
 
 void Bit2_free(Bit2_T *bit2)
 {
-    assert(bit2 != NULL);
-    Bit_free(&bit2->bit_arr);
-    bit2->bit_arr = NULL;
+    assert(bit2 != NULL && *bit2 != NULL);
+    Bit_free(&(*bit2)->bit_arr);
+    (*bit2)->bit_arr = NULL;
 
-    free(bit2);
-    bit2 = NULL;
+    free(*bit2);
+    *bit2 = NULL;
 }
 
 int Bit2_width(Bit2_T bit2)
 {
-    return bit2.cols;
+    assert(bit2 != NULL);
+    return bit2->cols;
 }
 
 int Bit2_height(Bit2_T bit2)
 {
-    return bit2.rows;
+    assert(bit2 != NULL);
+    return bit2->rows;
 }
 
 void Bit2_map_row_major(Bit2_T bit2, void apply(
     int row, int col, Bit2_T bit2, int value, void *cl), void *cl) 
 {
-	for (int r = 0; r < bit2.rows; r++) 
+    assert(bit2 != NULL);
+	for (int r = 0; r < bit2->rows; r++) 
     {
-        for (int c = 0; c < bit2.cols; c++) 
+        for (int c = 0; c < bit2->cols; c++) 
         {
             int value = Bit2_get(bit2, r, c);
             apply(r, c, bit2, value, cl);
@@ -73,12 +83,14 @@ void Bit2_map_row_major(Bit2_T bit2, void apply(
 void Bit2_map_col_major(Bit2_T bit2, void apply(
     int row, int col, Bit2_T bit2, int value, void *cl), void *cl) 
 {
-	for (int c = 0; c < bit2.cols; c++) 
+    assert(bit2 != NULL);
+	for (int c = 0; c < bit2->cols; c++) 
     {
-        for (int r = 0; r < bit2.rows; r++) 
+        for (int r = 0; r < bit2->rows; r++) 
         {
             int value = Bit2_get(bit2, r, c);
             apply(r, c, bit2, value, cl);
         }
     }
-} 
+}
+
--- bit2.h
+++ bit2.h
@@ -1,24 +1,17 @@
 #ifndef BIT2_INCLUDED
 #define BIT2_INCLUDED
 
-#include "bit.h"
-typedef struct Bit_T *bit;
+typedef struct Bit2_T *Bit2_T;
 
-typedef struct Bit2_T {
-    int rows;
-    int cols;
-    Bit_T bit_arr;
-} Bit2_T;
-
-Bit2_T Bit2_new(int rows, int cols);
-int Bit2_put(Bit2_T *bit2, int row, int col, int bit);
-int Bit2_get(Bit2_T bit2, int row, int col);
-void Bit2_free(Bit2_T *bit2);
-int Bit2_width(Bit2_T bit2);
-int Bit2_height(Bit2_T bit2);
-void Bit2_map_row_major(Bit2_T bit2, void apply(
-    int row, int col, Bit2_T bit2, int value, void *cl), void *cl);
-void Bit2_map_col_major(Bit2_T bit2, void apply(
-    int row, int col, Bit2_T bit2, int value, void *cl), void *cl);
+extern Bit2_T Bit2_new(int rows, int cols);
+extern int Bit2_put(Bit2_T bit2, int row, int col, int bit);
+extern int Bit2_get(Bit2_T bit2, int row, int col);
+extern void Bit2_free(Bit2_T *bit2);
+extern int Bit2_width(Bit2_T bit2);
+extern int Bit2_height(Bit2_T bit2);
+extern void Bit2_map_row_major(Bit2_T bit2, void apply(
+            int row, int col, Bit2_T bit2, int value, void *cl), void *cl);
+extern void Bit2_map_col_major(Bit2_T bit2, void apply(
+            int row, int col, Bit2_T bit2, int value, void *cl), void *cl);
 
 #endif
