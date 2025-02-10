1c0f186eb02dd2e9a2839cee283eb99d5650c33f
Euluer01 <144732266+Euluer01@users.noreply.github.com>
2025-02-10 16:49:57 -0500

    Add files via upload

--- bit2.c
+++ bit2.c
@@ -16,9 +16,9 @@
 
 // This struct represents a 2D bitmap using a 1D bit array from Hanson
 struct Bit2_T {
-    int rows;
-    int cols;
-    Bit_T bit_arr;
+        int rows;
+        int cols;
+        Bit_T bit_arr;
 };
 
 /* 
@@ -34,15 +34,15 @@ struct Bit2_T {
 */
 Bit2_T Bit2_new(int rows, int cols) 
 {
-    assert(rows > 0 && cols > 0);
-    Bit2_T bit2 = malloc(sizeof(*bit2)); // malloc space
-    assert(bit2 != NULL);
-    bit2->bit_arr = Bit_new(rows * cols); // use hanson bit
-    assert( bit2->bit_arr != NULL);
-    bit2->rows = rows;
-    bit2->cols = cols;
+        assert(rows > 0 && cols > 0);
+        Bit2_T bit2 = malloc(sizeof(*bit2)); // malloc space
+        assert(bit2 != NULL);
+        bit2->bit_arr = Bit_new(rows * cols); // use hanson bit
+        assert( bit2->bit_arr != NULL);
+        bit2->rows = rows;
+        bit2->cols = cols;
 
-    return bit2;
+        return bit2;
 }
 
 /*
@@ -58,13 +58,13 @@ Bit2_T Bit2_new(int rows, int cols)
 */
 int Bit2_put(Bit2_T bit2, int row, int col, int bit)
 {
-    assert(bit2 != NULL);
-    // make sure bounds are valid
-    assert(valid_index(bit2, row, col));
-    int index = row * bit2->cols + col;
-    int last_bit = Bit_put(bit2->bit_arr, index, bit);
+        assert(bit2 != NULL);
+        // make sure bounds are valid
+        assert(valid_index(bit2, row, col));
+        int index = row * bit2->cols + col;
+        int last_bit = Bit_put(bit2->bit_arr, index, bit);
     
-    return last_bit;
+        return last_bit;
 }
 
 /*
@@ -79,13 +79,13 @@ int Bit2_put(Bit2_T bit2, int row, int col, int bit)
 */
 int Bit2_get(Bit2_T bit2, int row, int col)
 {
-    assert(bit2 != NULL);
-    // make sure bounds are valid
-    assert(valid_index(bit2, row, col));
-    int index = row * bit2->cols + col;
-    int curr_bit = Bit_get(bit2->bit_arr, index);
+        assert(bit2 != NULL);
+        // make sure bounds are valid
+        assert(valid_index(bit2, row, col));
+        int index = row * bit2->cols + col;
+        int curr_bit = Bit_get(bit2->bit_arr, index);
     
-    return curr_bit;
+        return curr_bit;
 }
 
 /*
@@ -100,13 +100,13 @@ int Bit2_get(Bit2_T bit2, int row, int col)
 */
 void Bit2_free(Bit2_T *bit2)
 {
-    assert(bit2 != NULL || *bit2 != NULL);
-    if ((*bit2)->bit_arr != NULL) { // check before freeing
-        Bit_free(&(*bit2)->bit_arr);
-    }
+        assert(bit2 != NULL || *bit2 != NULL);
+        if ((*bit2)->bit_arr != NULL) { // check before freeing
+                Bit_free(&(*bit2)->bit_arr);
+        }
 
-    free(*bit2);
-    *bit2 = NULL;
+        free(*bit2);
+        *bit2 = NULL;
 }
 
 /*
@@ -119,8 +119,8 @@ void Bit2_free(Bit2_T *bit2)
 */
 int Bit2_width(Bit2_T bit2)
 {
-    assert(bit2 != NULL);
-    return bit2->cols;
+        assert(bit2 != NULL);
+        return bit2->cols;
 }
 
 /*
@@ -133,8 +133,8 @@ int Bit2_width(Bit2_T bit2)
 */
 int Bit2_height(Bit2_T bit2)
 {
-    assert(bit2 != NULL);
-    return bit2->rows;
+        assert(bit2 != NULL);
+        return bit2->rows;
 }
 
 /*
@@ -151,14 +151,14 @@ int Bit2_height(Bit2_T bit2)
 void Bit2_map_row_major(Bit2_T bit2, void apply(
     int row, int col, Bit2_T bit2, int value, void *cl), void *cl) 
 {
-    assert(bit2 != NULL);
-    for (int r = 0; r < bit2->rows; r++) {
-        for (int c = 0; c < bit2->cols; c++) {
-            int value = Bit2_get(bit2, r, c);
-            // pointer to a function
-            apply(r, c, bit2, value, cl);
+        assert(bit2 != NULL);
+        for (int r = 0; r < bit2->rows; r++) {
+                for (int c = 0; c < bit2->cols; c++) {
+                        int value = Bit2_get(bit2, r, c);
+                        // pointer to a function
+                        apply(r, c, bit2, value, cl);
+                }
         }
-    }
 }
 
 /*
@@ -175,14 +175,14 @@ void Bit2_map_row_major(Bit2_T bit2, void apply(
 void Bit2_map_col_major(Bit2_T bit2, void apply(
     int row, int col, Bit2_T bit2, int value, void *cl), void *cl) 
 {
-    assert(bit2 != NULL);
-    for (int c = 0; c < bit2->cols; c++) {
-        for (int r = 0; r < bit2->rows; r++) {
-            int value = Bit2_get(bit2, r, c);
-            // pointer to a function
-            apply(r, c, bit2, value, cl);
+        assert(bit2 != NULL);
+        for (int c = 0; c < bit2->cols; c++) {
+                for (int r = 0; r < bit2->rows; r++) {
+                        int value = Bit2_get(bit2, r, c);
+                        // pointer to a function
+                        apply(r, c, bit2, value, cl);
+                }
         }
-    }
 }
 
 /*
@@ -197,7 +197,7 @@ void Bit2_map_col_major(Bit2_T bit2, void apply(
 */
 int valid_index(Bit2_T bit2, int row, int col)
 {
-    assert(bit2 != NULL);
-    return (row >= 0 && row < Bit2_height(bit2) &&
-            col >= 0 && col < Bit2_width(bit2));
+        assert(bit2 != NULL);
+        return (row >= 0 && row < Bit2_height(bit2) &&
+                col >= 0 && col < Bit2_width(bit2));
 }
--- unblackedges.c
+++ unblackedges.c
@@ -13,13 +13,20 @@
 #include <stdio.h>
 #include "assert.h"
 #include "pnmrdr.h"
+#include "stack.h"
+
+// These are entries of black pixels to put in a stack.
+typedef struct {
+        int row, col;
+} BlackPixels;
 
 
 Bit2_T pbmread(FILE *inputfp);
 void   pbmwrite(Bit2_T bitmap);
-void   swap(Bit2_T bitmap, int row, int col);
-void   edges(Bit2_T bitmap);
-void   unblackedges(Bit2_T bitmap, int row, int col);
+void   swap_color(Stack_T pixels, Bit2_T bitmap);
+void   push_pixels(Stack_T pixels, Bit2_T bitmap, int row, int col);
+void   unblackedges(Bit2_T bitmap);
+void   edgepix_to_stack(Stack_T pixels, Bit2_T bitmap, int row, int col);
 
 /*
 *  name:        main
@@ -40,29 +47,29 @@ void   unblackedges(Bit2_T bitmap, int row, int col);
 */
 int main(int argc, char *argv[])
 {   
-    if (argc == 2) { //read from a file   
-        FILE *inputfp = fopen(argv[1], "r");
-        assert(inputfp != NULL);
+        if (argc == 2) { //read from a file   
+                FILE *inputfp = fopen(argv[1], "r");
+                assert(inputfp != NULL);
 
-        Bit2_T bitmap = pbmread(inputfp);
-        assert(bitmap != NULL);
-        fclose(inputfp);
-        edges(bitmap);
-        pbmwrite(bitmap);
-        Bit2_free(&bitmap); // free         
-    } else if (argc == 1) { //read from standard input
-        Bit2_T bitmap = pbmread(stdin);
-        assert(bitmap != NULL);
-        edges(bitmap);
-        pbmwrite(bitmap);
-        Bit2_free(&bitmap); // free
-    } else {
-        // there should only ever be max two arguments
-        printf("Too many arguments\n");
-        exit(EXIT_FAILURE);
-    }
+                Bit2_T bitmap = pbmread(inputfp);
+                assert(bitmap != NULL);
+                fclose(inputfp);
+                unblackedges(bitmap);
+                pbmwrite(bitmap);
+                Bit2_free(&bitmap); // free         
+        } else if (argc == 1) { //read from standard input
+                Bit2_T bitmap = pbmread(stdin);
+                assert(bitmap != NULL);
+                unblackedges(bitmap);
+                pbmwrite(bitmap);
+                Bit2_free(&bitmap); // free
+        } else {
+                // there should only ever be max two arguments
+                printf("Too many arguments\n");
+                exit(EXIT_FAILURE);
+        }
         
-    return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
 
 /*
@@ -77,109 +84,92 @@ int main(int argc, char *argv[])
 */
 Bit2_T pbmread(FILE *inputfp)
 {
-    assert(inputfp != NULL);
+        assert(inputfp != NULL);
 
-    // get the information of the pbm by using pnmrdr
-    Pnmrdr_T rdr = Pnmrdr_new(inputfp);
-    Pnmrdr_mapdata map = Pnmrdr_data(rdr);
+        // get the information of the pbm by using pnmrdr
+        Pnmrdr_T rdr = Pnmrdr_new(inputfp);
+        Pnmrdr_mapdata map = Pnmrdr_data(rdr);
 
-    Bit2_T bitmap = Bit2_new(map.height, map.width);
+        Bit2_T bitmap = Bit2_new(map.height, map.width);
 
-    // make sure to read the whole file
-    for (unsigned row = 0; row < map.height; row++) {
-        for (unsigned col = 0; col < map.width; col++) {
-            int bit = Pnmrdr_get(rdr);
-            Bit2_put(bitmap, row, col, bit);
+        // make sure to read the whole file
+        for (unsigned row = 0; row < map.height; row++) {
+                for (unsigned col = 0; col < map.width; col++) {
+                    int bit = Pnmrdr_get(rdr);
+                    Bit2_put(bitmap, row, col, bit);
+                }
         }
-    }
 
-    Pnmrdr_free(&rdr);
-    return bitmap;
+        Pnmrdr_free(&rdr);
+        return bitmap;
 }
 
 /*
-*  name:        swap
-*  purpose:     Sets the bit at a specified row and column to 0.
-*  arguments:   A Bit2_T representing the bitmap, an integer row index, 
-*               and an integer column index.
+*  name:        swap_color
+*  purpose:     Iterates through a stack of black pixels and changes them
+*               to white (0), ensuring all connected black pixels are also
+*               processed.
+*  arguments:   A stack containing black pixels to process and a bitmap
+*               representing the 2D bit array.
 *  return type: None.
-*  effect:      Modifies the bit at the specified position by setting it to 0.
-*  expects:     The bitmap pointer is not NULL, and row/column indices 
-*               are within valid bounds.
+*  effect:      Iterates through the stack and changes black pixels (1) to
+*               white (0) in the bit map. Pushes adjacent black pixels to
+*               the stack for further processing. Frees dynamically allocated
+*               memory for each processed pixel, since they are allocated
+*               structs.
+*  expects:     Bitmap and pixels are not NULL and the pixels in the stack
+*               contain valid row and column indices.
 */
-void swap(Bit2_T bitmap, int row, int col)
+void swap_color(Stack_T pixels, Bit2_T bitmap)
 {
-    assert(bitmap != NULL);
-    assert(valid_index(bitmap, row, col));
-    Bit2_put(bitmap, row, col, 0);
-}
+        assert(bitmap != NULL && pixels != NULL);
+    
+        while (!Stack_empty(pixels)) {
+                BlackPixels *curr_pixel = Stack_pop(pixels);
+                int row  = curr_pixel->row;
+                int col = curr_pixel->col;
+                free(curr_pixel);
 
-/*
-*  name:        edges
-*  purpose:     Identifies black pixels (1s) on the edges of the bitmap 
-*               and removes them by calling unblackedges.
-*  arguments:   A Bit2_T representing the bitmap.
-*  return type: None.
-*  effect:      Modifies the bitmap by changing edge-connected black pixels
-*               to white (0).
-*  expects:     The bitmap pointer is not NULL.
-*/
-void edges(Bit2_T bitmap)
-{
-    assert(bitmap != NULL);
-    int height = Bit2_height(bitmap);
-    int width = Bit2_width(bitmap);
-
-    for (int col = 0; col < width; col++) { // top edge
-        if (Bit2_get(bitmap, 0, col) == 1) {
-            unblackedges(bitmap, 0, col);
+                if (Bit2_get(bitmap, row, col) == 1) {  
+                        Bit2_put(bitmap, row, col, 0);
+                        push_pixels(pixels, bitmap, row, col);
+                }
         }
-    }
-
-    for (int col = 0; col < width; col++) { // bottom edge
-        if (Bit2_get(bitmap, height - 1, col) == 1) {
-            unblackedges(bitmap, height - 1, col);
-        }
-    }
-
-    for (int row = 1; row < height - 1; row++) { // left edge
-        if (Bit2_get(bitmap, row, 0) == 1) {
-            unblackedges(bitmap, row, 0);
-        }
-    }
-
-    for (int row = 1; row < height - 1; row++) { // right edge
-        if (Bit2_get(bitmap, row, width - 1) == 1) {
-            unblackedges(bitmap, row, width - 1);
-        }
-    }
+    
 }
 
 /*
 *  name:        unblackedges
-*  purpose:     Recursively removes black pixels (1s) that are connected 
-*               to the edges of the bitmap.
-*  arguments:   A Bit2_T representing the bitmap, an integer row index, 
-*               and an integer column index.
+*  purpose:     Iteratively removes black pixels (1s) that are connected 
+*               to the edges of the bitmap using a stack.
+*  arguments:   A bitmap representing the 2D bit array.
 *  return type: None.
-*  effect:      Modifies the bitmap by turning black edge-connected pixels into white (0).
-*  expects:     The bitmap pointer is not NULL, and row/column indices are valid.
+*  effect:      Allocates memory for a new stack and then frees it
+*               at the end. Calls other functions that allocate memory, but
+*               that will also be freed in their respective functions.
+*  expects:     The bitmap pointer is not NULL.
 */
-void unblackedges(Bit2_T bitmap, int row, int col)
+void unblackedges(Bit2_T bitmap)
 {
-    assert(bitmap != NULL);    
+        assert(bitmap != NULL);
+        Stack_T pixels = Stack_new(); // create the stack
+        int width = Bit2_width(bitmap);
+        int height = Bit2_height(bitmap);
 
-    if (valid_index(bitmap, row, col)) {
-        if (Bit2_get(bitmap, row, col) == 1) {
-            swap(bitmap, row, col);
-
-            // use unblackedges on down, up, left, right
-            unblackedges(bitmap, row - 1, col);
-            unblackedges(bitmap, row + 1, col);
-            unblackedges(bitmap, row, col - 1);
-            unblackedges(bitmap, row, col + 1);
+        // top and bottom
+        for (int col = 0; col < width; col++) {
+                edgepix_to_stack(pixels, bitmap, 0, col);
+                edgepix_to_stack(pixels, bitmap, height - 1, col);
         }
-    }
+
+        // sides no overlap with the corners
+        for (int row = 1; row < height - 1; row++) {
+                edgepix_to_stack(pixels, bitmap, row, 0);
+                edgepix_to_stack(pixels, bitmap, row, width - 1);
+        }
+
+        swap_color(pixels, bitmap); // process our stack
+        Stack_free(&pixels); // free the stack
 }
 
 /*
@@ -190,21 +180,87 @@ void unblackedges(Bit2_T bitmap, int row, int col)
 *  effect:      Prints the PBM representation of the bitmap to stdout.
 *  expects:     The bitmap pointer is not NULL.
 */
-void pbmwrite(Bit2_T bitmap){
-    assert(bitmap != NULL);
-    int width = Bit2_width(bitmap);
-    int height = Bit2_height(bitmap);
+void pbmwrite(Bit2_T bitmap)
+{
+        assert(bitmap != NULL);
+        int width = Bit2_width(bitmap);
+        int height = Bit2_height(bitmap);
 
-    // print the header
-    printf("P1\n");
-    printf("%d %d\n", width, height);
+        // print the header
+        printf("P1\n");
+        printf("%d %d\n", width, height);
 
-    // print the data
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
-            printf("%d ", Bit2_get(bitmap, row, col));
+        // print the data
+        for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                        printf("%d ", Bit2_get(bitmap, row, col));
+                }
+
+                printf("\n");
+        }
+}
+
+/*
+*  name:        push_pixels
+*  purpose:     Identifies the four adjacent pixels (up, down, left, right) 
+*               and pushes them onto the stack for processing.
+*  arguments:   A stack containing black pixels to process and a bitmap
+*               representing the 2D bit array, as well as two ints
+*               represnting an index in the bitmap.
+*  return type: None.
+*  effect:      Dynamically allocates memory for up to four BlackPixels
+*               structs, then pushes those onto a stack. If one is invalid
+*               it is freed, otherwise the clients need to call swap_color
+*               to free the rest in the stack.
+*  expects:     Pixels and bitmap are not NULL. and row + col are valid bounds
+*/
+void push_pixels(Stack_T pixels, Bit2_T bitmap, int row, int col)
+{
+        assert(pixels != NULL && bitmap != NULL);
+
+        BlackPixels *connected[4];
+        for (int i = 0; i < 4; i++) {
+                connected[i] = malloc(sizeof(BlackPixels));
+                assert(connected[i] != NULL);
         }
 
-        printf("\n");
-    }
-}    
+        connected[0]->row = row - 1; // up
+        connected[0]->col = col; // up
+        connected[1]->row = row + 1; // down
+        connected[1]->col = col;  // down
+        connected[2]->row = row; // left
+        connected[2]->col = col - 1; // left
+        connected[3]->row = row; // right
+        connected[3]->col = col + 1; // right
+
+        for (int i = 0; i < 4; i++) { // make sure they're in bounds
+                if (valid_index(bitmap, connected[i]->row, connected[i]->col)) {
+                        Stack_push(pixels, connected[i]);
+                } else {
+                        free(connected[i]); // free if not
+                }
+        }
+}
+
+/*
+*  name:        edgepix_to_stack
+*  purpose:     Checks if a given edge pixel is black (1) and pushes
+*               it onto the stack.
+*  arguments:   A stack containing black pixels to process and a bitmap
+*               representing the 2D bit array, as well as two ints
+*               represnting an index in the bitmap.
+*  return type: None.
+*  effect:      Dynamically allocates space fro BlackPixel structs if
+*               an edge is black and pushes it onto a stack. These
+*               will be freed when swap_color is called.
+*  expects:     Pixels and bitmap are not NULL. and row + col are valid bounds
+*/
+void edgepix_to_stack(Stack_T pixels, Bit2_T bitmap, int row, int col)
+{
+        if (Bit2_get(bitmap, row, col) == 1) {
+                BlackPixels *curr_pixel = malloc(sizeof(BlackPixels));
+                curr_pixel->row = row;
+                curr_pixel->col = col;
+                Stack_push(pixels, curr_pixel);
+        }
+}
